62006343fdaa1229cd7d5ba7e69b6face6cb4642
Madiyar Umirzakov <m.umirzakov@brokerage24.kz>
2022-05-02 16:12:54 +0600

    minor fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,33 +2,50 @@ import React from "react";
 import { Layout, Row, Col, Avatar, Dropdown } from "antd";
 
 import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    UserOutlined
-} from '@ant-design/icons';
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import profileMenu from "./profilemenu";
 import { useSelector } from "react-redux";
+import { useGetAccountQuery } from "../../app/api/auth";
 
 const { Header } = Layout;
 
-const MainHeader = ({toggle, collapsed}) => {
-    const { user } = useSelector(state => state.user);
+const MainHeader = ({ toggle, collapsed }) => {
+  const { data: userData, refetch } = useGetAccountQuery();
+  const { user } = useSelector((state) => state.user);
 
+  console.log(userData);
 
-    return (
-        <Header className="site-layout-background" style={{ padding: 0 }}>
-            <Row>
-                <Col span={12}>
-                { collapsed ? <MenuUnfoldOutlined className="trigger" onClick={toggle} /> 
-                            : <MenuFoldOutlined className="trigger" onClick={toggle} /> }
-                </Col>
-                <Col span={12} style={{textAlign:"right"}}>
-                    <Dropdown overlay={profileMenu} placement="bottomCenter" arrow>  
-                        <Avatar shape="square" size={32} style={{marginRight:20}} icon={<UserOutlined />} />
-                    </Dropdown>
-                </Col>
-            </Row>
-        </Header>
-    )
-}
+  return (
+    <Header className="site-layout-background" style={{ padding: 0 }}>
+      <Row>
+        <Col span={12}>
+          {collapsed ? (
+            <MenuUnfoldOutlined className="trigger" onClick={toggle} />
+          ) : (
+            <MenuFoldOutlined className="trigger" onClick={toggle} />
+          )}
+        </Col>
+
+        <Col span={12} style={{ textAlign: "right" }}>
+          <Dropdown overlay={profileMenu} placement="bottomCenter" arrow>
+            <Avatar
+              shape="square"
+              size={32}
+              style={{ marginRight: 20 }}
+              icon={
+                <span>
+                  {" "}
+                  <p> {userData?.displayName} </p> <UserOutlined />
+                </span>
+              }
+            />
+          </Dropdown>
+        </Col>
+      </Row>
+    </Header>
+  );
+};
 export default MainHeader;
--- src/components/header/profilemenu.js
+++ src/components/header/profilemenu.js
@@ -1,18 +1,25 @@
 import { Menu } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const onClick = ({ key }) => {
-  
+
+  console.log('clicked', key)
+
   if(key === 'exit') {
     localStorage.removeItem('token');
     window.location.reload()
+  }
+  else {
+    navigate(key);
   }
 };
 
 
 const profileMenu = (
+  
     <Menu onClick={onClick}>
-      <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer">
+      {/* <Menu.Item key='/profile'>
+        <a target="/settings" rel="noopener noreferrer">
           Профиль
         </a>
       </Menu.Item>
@@ -20,7 +27,7 @@ const profileMenu = (
         <a target="_blank" rel="noopener noreferrer" >
           Настройка
         </a>
-      </Menu.Item>
+      </Menu.Item> */}
       <Menu.Item key='exit'>
         <div>
           Выйти
